27cad25d6caef0b427c5849c94365f91de05890a
Arshidin <smartmope@gmail.com>
2026-01-12 22:35:56 +0500

    fix: Add missing BalanceModule and ProvidersModule imports to SubscriptionsModule

--- checkie-backend/src/modules/subscriptions/subscriptions.module.ts
+++ checkie-backend/src/modules/subscriptions/subscriptions.module.ts
@@ -3,9 +3,16 @@ import { SubscriptionsController } from './subscriptions.controller';
 import { SubscriptionsService } from './subscriptions.service';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { WebhooksModule } from '../webhooks/webhooks.module';
+import { BalanceModule } from '../balance/balance.module';
+import { ProvidersModule } from '../providers/providers.module';
 
 @Module({
-  imports: [PrismaModule, forwardRef(() => WebhooksModule)],
+  imports: [
+    PrismaModule,
+    forwardRef(() => WebhooksModule),
+    BalanceModule,
+    ProvidersModule,
+  ],
   controllers: [SubscriptionsController],
   providers: [SubscriptionsService],
   exports: [SubscriptionsService],
